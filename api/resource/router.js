69e4ae9c9924c7b93b2b82b0b3822de09df99304
dwilliams304 <d.williams0434@gmail.com>
2023-10-06 09:24:40 -0700

    Migrations

--- api/resource/router.js
+++ api/resource/router.js
@@ -1,1 +1,18 @@
 // build your `/api/resources` router here
+const express = require('express');
+
+const router = express.Router();
+
+
+router.post('/', (req, res, next) => {
+
+})
+
+
+router.get('/', (req, res, next) => {
+
+})
+
+
+
+module.exports = router;
---
+++ data/migrations/create-tables.js
@@ -0,0 +1,31 @@
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('projects', tbl => {
+            tbl.increments('project_id');
+            tbl.string('project_name').notNullable();
+            tbl.string('project_description');
+            tbl.boolean('project_completed').defaultTo(false);
+        })
+        .createTable('resources', tbl => {
+            tbl.increments('resource_id');
+            tbl.string('resource_name').notNullable().unique();
+            tbl.string('resource_description');
+        })
+        .createTable('tasks', tbl => {
+            tbl.increments('task_id');
+            tbl.string('task_description').notNullable();
+            tbl.string('task_notes');
+            tbl.boolean('task_completed').defaultTo(false);
+            tbl.integer('project_id').references('project_id').inTable('projects');
+        })
+
+}
+
+
+exports.down = function(knex){
+    return knex.schema
+        .dropTableIfExists('projects')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('tasks');
+
+}
